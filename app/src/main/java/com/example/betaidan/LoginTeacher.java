6802eaa97f070a4fe225b538c86b63eac6a0d10d
user <hadasty1@gmail.com>
2020-04-08 14:12:37 +0300

    Working Reg + Location, gotta fix Teacher Activity crashing while pressing on the Order Offer

--- app/src/main/java/com/example/betaidan/LoginTeacher.java
+++ app/src/main/java/com/example/betaidan/LoginTeacher.java
@@ -1,14 +1,191 @@
 package com.example.betaidan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+
+import static com.example.betaidan.FBref.refAuth;
+
+import static com.example.betaidan.FBref.refTeacher;
 
 public class LoginTeacher extends AppCompatActivity {
+
+    TextView tVtitle, tVregister;
+    EditText eTname, eTphone, eTemail, eTpass,eTexp,eTab;
+    CheckBox cBstayconnect;
+    Button btn;
+    boolean yes = false;
+    String name, phone, email, password, uid,Experience,About;
+    Teacher teacher;
+    Boolean stayConnect, registered, firstrun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_teacher);
+        tVtitle=(TextView) findViewById(R.id.tVtitle);
+        eTname=(EditText)findViewById(R.id.eTname);
+        eTexp=(EditText)findViewById(R.id.eTexp);
+        eTab=(EditText)findViewById(R.id.eTab);
+        eTemail=(EditText)findViewById(R.id.eTemail);
+        eTpass=(EditText)findViewById(R.id.eTpass);
+        eTphone=(EditText)findViewById(R.id.eTphone);
+        cBstayconnect=(CheckBox)findViewById(R.id.cBstayconnect);
+        tVregister=(TextView) findViewById(R.id.tVregister);
+        btn=(Button)findViewById(R.id.btn);
+
+        stayConnect=false;
+        registered=true;
+
+        regoption();
     }
+
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences settings=getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
+        Boolean isChecked=settings.getBoolean("stayConnect",false);
+        Intent si = new Intent(LoginTeacher.this,TeacherActivity.class);
+        if (refAuth.getCurrentUser()!=null && isChecked) {
+            stayConnect=true;
+            startActivity(si);
+        }
+
+
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (stayConnect) finish();
+    }
+
+    private void regoption() {
+        SpannableString ss = new SpannableString("Don't have an account?  Register here!");
+        ClickableSpan span = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                tVtitle.setText("Register");
+                eTname.setVisibility(View.VISIBLE);
+                eTphone.setVisibility(View.VISIBLE);
+                eTexp.setVisibility(View.VISIBLE);
+                eTab.setVisibility(View.VISIBLE);
+
+                btn.setText("Register");
+                registered=false;
+                logoption();
+            }
+        };
+        ss.setSpan(span, 24, 38, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tVregister.setText(ss);
+        tVregister.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void logoption() {
+        SpannableString ss = new SpannableString("Already have an account?  Login here!");
+        ClickableSpan span = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                tVtitle.setText("Login");
+                eTname.setVisibility(View.INVISIBLE);
+                eTphone.setVisibility(View.INVISIBLE);
+                eTexp.setVisibility(View.INVISIBLE);
+                eTab.setVisibility(View.INVISIBLE);
+                btn.setText("Login");
+                registered=true;
+                regoption();
+            }
+        };
+        ss.setSpan(span, 26, 37, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tVregister.setText(ss);
+        tVregister.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    public void logorreg(View view) {
+        if (registered) {
+            email=eTemail.getText().toString();
+            password=eTpass.getText().toString();
+
+            final ProgressDialog pd=ProgressDialog.show(this,"Login","Connecting...",true);
+            refAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            pd.dismiss();
+                            if (task.isSuccessful()) {
+                                SharedPreferences settings=getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
+                                SharedPreferences.Editor editor=settings.edit();
+                                editor.putBoolean("stayConnect",cBstayconnect.isChecked());
+                                editor.commit();
+                                Log.d("MainActivity", "signinUserWithEmail:success");
+                                Toast.makeText(LoginTeacher.this, "Login Success", Toast.LENGTH_LONG).show();
+                                Intent si = new Intent(LoginTeacher.this,TeacherActivity.class);
+                                startActivity(si);
+                            } else {
+                                Log.d("MainActivity", "signinUserWithEmail:fail");
+                                Toast.makeText(LoginTeacher.this, "e-mail or password are wrong!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        } else {
+            name=eTname.getText().toString();
+            phone=eTphone.getText().toString();
+            email=eTemail.getText().toString();
+            password=eTpass.getText().toString();
+            Experience=eTexp.getText().toString();
+            About=eTab.getText().toString();
+
+            final ProgressDialog pd=ProgressDialog.show(this,"Register","Registering...",true);
+            refAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            pd.dismiss();
+                            if (task.isSuccessful()) {
+                                SharedPreferences settings=getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
+                                SharedPreferences.Editor editor=settings.edit();
+                                editor.putBoolean("stayConnect",cBstayconnect.isChecked());
+                                editor.commit();
+                                Log.d("MainActivity", "createUserWithEmail:success");
+                                FirebaseUser user = refAuth.getCurrentUser();
+                                uid = user.getUid();
+                               teacher=new Teacher( name, phone, email, Experience, About, uid,  yes);
+                                refTeacher.child(uid).setValue(teacher);
+                                Toast.makeText(LoginTeacher.this, "Successful registration", Toast.LENGTH_LONG).show();
+                                Intent si = new Intent(LoginTeacher.this,TeacherActivity.class);
+                                startActivity(si);
+                            } else {
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException)
+                                    Toast.makeText(LoginTeacher.this, "User with e-mail already exist!", Toast.LENGTH_LONG).show();
+                                else {
+                                    Log.w("MainActivity", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginTeacher.this, "User create failed.",Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/betaidan/TeacherActivity.java
+++ app/src/main/java/com/example/betaidan/TeacherActivity.java
@@ -1,14 +1,206 @@
 package com.example.betaidan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class TeacherActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import static com.example.betaidan.FBref.refLocations;
+import static com.example.betaidan.FBref.refstudent;
+import static com.example.betaidan.R.layout.studentdial;
+
+public class TeacherActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView lv;
+    Button btn;
+    TextView tvname  ,tvPhone,tvAbout,tvexp,tv5;
+    String address;
+    String name,phone,Experience,About;
+    AlertDialog.Builder adb;
+    Intent t;
+    LinearLayout studentdialog1;
+    Dialog studentdialog12;
+    Student student;
+    FusedLocationProviderClient fusedLocationProviderClient ;
+    ArrayList<String> offer=new ArrayList<>();
+    ArrayList<LocationObject> locationObjects2 = new ArrayList<>();
+    ArrayAdapter<String> adp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+        lv=(ListView) findViewById(R.id.LV1);
+        lv.setOnItemClickListener(TeacherActivity.this);
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        adp = new ArrayAdapter<String>(TeacherActivity.this, R.layout.support_simple_spinner_dropdown_item, offer);
+
+        lv.setAdapter(adp);
+
+
     }
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+        start();
+    };
+
+
+
+    ValueEventListener locationListener;{
+        locationListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                offer.clear();
+                locationObjects2.clear();
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    String firstAddress = (String) data.getKey();
+                    LocationObject LocationObject = data.getValue(LocationObject.class);
+                    locationObjects2.add(LocationObject);
+                    String Location = LocationObject.getMyLocation();
+                    String Subject = LocationObject.getSubject();
+                    offer.add("I am at:  " + Location + " and my subject is: " + Subject);
+
+                }
+
+                adp = new ArrayAdapter<String>(TeacherActivity.this,
+                        R.layout.support_simple_spinner_dropdown_item, offer);
+                lv.setAdapter(adp);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("TeacherActivity", "Failed to read value", databaseError.toException());
+
+            }
+
+        };
+        refLocations.addValueEventListener(locationListener);
+
+    }
+    DialogInterface.OnClickListener myclick= new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (which == DialogInterface.BUTTON_POSITIVE) {
+                getLocation();
+                Intent t=new Intent(TeacherActivity.this,mapforTeacher.class);
+                Toast.makeText(TeacherActivity.this,"Successful",Toast.LENGTH_SHORT).show();
+            }
+            if (which == DialogInterface.BUTTON_NEGATIVE) {
+                dialog.cancel();
+
+            }
+        }
+
+    };
+    @SuppressLint("CutPasteId")
+    public void start() {
+        ValueEventListener studentdetails;
+
+        studentdialog1 = (LinearLayout) getLayoutInflater().inflate(studentdial, null);
+        adb = new AlertDialog.Builder(this);
+        adb.setView(studentdialog1);
+        adb.setTitle("customer details  ");
+        tvname = (TextView) findViewById(R.id.tvnamee);
+        tvPhone = (TextView) findViewById(R.id.tvPhonee);
+        tvAbout = (TextView) findViewById(R.id.tvAbout);
+        tvexp = (TextView) findViewById(R.id.tvexp);
+        adb.setPositiveButton("enter", (DialogInterface.OnClickListener) myclick);
+        adb.setNegativeButton("cancel", (DialogInterface.OnClickListener) myclick);
+        adb.show();
+        studentdetails = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()) {
+                    String uid = (String) data.getKey();
+                    Student c = data.getValue(Student.class);
+
+                    name = c.getName();
+                    phone = c.getPhone();
+                    Experience = c.getStudentClass();
+                    tvname.setText(name);
+                    tvPhone.setText(phone);
+                    tvexp.setText(Experience);
+                    tvAbout.setText(About);
+
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("TeacherActivity", "Failed to read value", databaseError.toException());
+            }
+
+
+        };refstudent.addValueEventListener(studentdetails);
+
+
+    }
+
+    private void getLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                // Intiialize location
+                Location location = task.getResult();
+                if (location!=null){
+                    //Intiialize geoCoder
+                    Geocoder geocoder = new Geocoder(TeacherActivity.this,
+                            Locale.getDefault());
+                    // Intiialize address list
+                    try {
+                        List<Address> addresses= geocoder.getFromLocation(
+                                location.getLatitude(),location.getLongitude(),1
+                        );
+                        //set address
+                        tv5.setText(Html.fromHtml(
+                                "<font color =' #6200EE'><b>Address:</b><br></fonnt>"
+                                        + addresses.get(0).getAddressLine(0))
+                        );
+
+
+                    } catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }
+        });
+        address = tv5.getText().toString();
+        Toast.makeText(TeacherActivity.this, address, Toast.LENGTH_LONG).show();
+    }
+
 }
